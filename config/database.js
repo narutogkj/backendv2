9ff986ad031b2143ac2f5235704b730a3583b379
narutogkj <gkj498@gmail.com>
2020-12-12 12:59:20 +0530

    second

--- config/database.js
+++ config/database.js
@@ -1,11 +1,11 @@
 const { createPool } = require("mysql");
 
 const pool = createPool({
-    host: process.env.MYSQL_HOST,
-    port: process.env.MYSQL_PORT,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
+    host: process.env.MYSQL_ADDON_HOST,
+    port: process.env.MYSQL_ADDON_PORT,
+    user: process.env.MYSQL_ADDON_USER,
+    password: process.env.MYSQL_ADDON_PASSWORD,
+    database: process.env.MYSQL_ADDON_DB
 });
 
 module.exports = pool;
